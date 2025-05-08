2bd2c50b94ee7974b6f96fd640130556ddf070df
cesarcampos_c <camposcontreras.cesar@gmail.com>
2025-05-07 20:50:20 -0500

    Product Details Screen

--- screens/ProductDetailsScreen.js
+++ screens/ProductDetailsScreen.js
@@ -1,4 +1,4 @@
-import { FlatList, Image, StyleSheet, Text, View, useWindowDimensions} from 'react-native'
+import { FlatList, Image, Pressable, ScrollView, StyleSheet, Text, View, useWindowDimensions} from 'react-native'
 import React from 'react'
 import products from '../data/products'
 
@@ -7,29 +7,42 @@ const ProductDetailsScreen = () => {
 
     const {width} = useWindowDimensions();
 
+    const addToCart = () => {
+      console.warn('Add to cart')
+    }
+
   return (
-    <View >
-      {/* Image Carousel */}
-      <FlatList
-        data={product.images}
-        renderItem={({ item }) => (
-          <Image
-            source={{ uri: item }}
-            style={{ width: width, aspectRatio: 1 }}
-          />
-        )}
-        horizontal
-        showsHorizontalScrollIndicator = {false}
-        pagingEnabled
-      />
+    <View>
+      <ScrollView>
+        {/* Image Carousel */}
+        <FlatList
+          data={product.images}
+          renderItem={({ item }) => (
+            <Image
+              source={{ uri: item }}
+              style={{ width: width, aspectRatio: 1 }}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+        />
 
-      {/* Title */}
+        <View style={{ padding: 20 }}>
+          {/* Title */}
+          <Text style={styles.title}>{product.name}</Text>
 
-      {/* Price */}
+          {/* Price */}
+          <Text style={styles.price}>$ {product.price}</Text>
 
-      {/* Description */}
-
+          {/* Description */}
+          <Text style={styles.description}>{product.description}</Text>
+        </View>
+      </ScrollView>
       {/* add to Cart button */}
+      <Pressable style={styles.button} onPress={addToCart}>
+        <Text style={styles.buttonText}>ADD To Cart</Text>
+      </Pressable>
     </View>
   );
 }
@@ -37,8 +50,39 @@ const ProductDetailsScreen = () => {
 export default ProductDetailsScreen
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'azure',
+    title: {
+      fontSize: 30,
+      fontWeight: '600',
+      marginVertical: 10,
+    },
+
+    price: {
+      fontWeight: '600',
+      fontSize: 18,
+      letterSpacing: 2,
+    },
+
+    description: {
+      marginVertical: 10,
+      fontSize: 20,
+      lineHeight: 30,
+      fontWeight: '400'
+    },
+
+    button: {
+      position: 'absolute',
+      backgroundColor: 'red',
+      bottom: 30,
+      width: '90%',
+      alignSelf: 'center',
+      padding: 25,
+      borderRadius: 50,
+      alignItems: 'center'
+    },
+
+    buttonText: {
+      color: 'white',
+      fontSize: 20,
+      fontWeight: '600'
     }
 })
